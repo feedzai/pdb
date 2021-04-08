9de86a2e373da9b513287b6adbb690eb3b45d582
Francisco Santos <francisco.santos@feedzai.com>
2021-04-08 12:57:41 +0100

    PDB-24: Implement connection pool

---
+++ src/main/java/com/feedzai/commons/sql/abstraction/engine/pool/DatabaseEnginePool.java
@@ -0,0 +1,263 @@
+/*
+ * Copyright 2021 Feedzai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feedzai.commons.sql.abstraction.engine.pool;
+
+import com.google.common.collect.Maps;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.function.Consumer;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.impl.AbandonedConfig;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.feedzai.commons.sql.abstraction.engine.DatabaseEngine;
+
+import static java.util.stream.Collectors.groupingBy;
+
+/**
+ * A pool of {@link DatabaseEngine} instances.
+ *
+ * @author Luiz Silva (luiz.silva@feedzai.com)
+ * @author Francisco Santos (francisco.santos@feedzai.com)
+ * @since 2.8.3
+ */
+public class DatabaseEnginePool implements AutoCloseable {
+
+    /**
+     * The logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseEnginePool.class);
+
+    /**
+     * The actual pool.
+     */
+    private final ObjectPool<PooledDatabaseEngine> pool;
+
+    /**
+     * Pool name for logging purposes.
+     */
+    private final String poolName;
+
+    /**
+     * Pool JDBC string for testing purposes.
+     */
+    private final String poolJdbc;
+
+    /**
+     * The maximum number of connections.
+     */
+    private final int maxTotal;
+
+    /**
+     * Creates a new {@link DatabaseEnginePool}.
+     *
+     * @param properties the configured database and pool properties.
+     * @param engineModifier the database engine modifier to fit the application needs.
+     */
+    private DatabaseEnginePool(final Map<String, String> properties, final Consumer<DatabaseEngine> engineModifier) {
+
+        // The properties object comes from the configuration file and are passed to the factory, which in turn
+        // is passed to the PDB database engine factory.
+        final Map<String, List<Entry<String, String>>> poolEntriesGroupedByPrefix = properties
+                .entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith("pool.generic.")
+                        || entry.getKey().startsWith("pool.abandoned."))
+                .collect(groupingBy(entry -> entry.getKey().split("\\.")[1]));
+
+        final List<Entry<String, String>> genericProps = poolEntriesGroupedByPrefix.get("generic");
+        final GenericObjectPoolConfig<PooledDatabaseEngine> genericConfig = genericProps == null
+                ? new GenericObjectPoolConfig<>()
+                : applyPrefixedConfig("pool.generic.", genericProps, new GenericObjectPoolConfig<>());
+
+
+        final List<Entry<String, String>> abandonedProps = poolEntriesGroupedByPrefix.get("abandoned");
+        final AbandonedConfig abandonedConfig = abandonedProps == null ? null
+                : applyPrefixedConfig("pool.abandoned.", abandonedProps, new AbandonedConfig());
+
+        final PooledDatabaseEngineFactory factory = new PooledDatabaseEngineFactory(properties, engineModifier);
+        this.pool = new GenericObjectPool<>(factory, genericConfig, abandonedConfig);
+        // format to have a pretty name.
+        this.poolName = factory.getClass().getSimpleName().replace("Factory", "Pool");
+        this.poolJdbc = properties.get("pdb.jdbc");
+        this.maxTotal = genericConfig.getMaxTotal();
+        LOGGER.info("'{}' has max size of {}", poolName, maxTotal);
+
+        factory.setPool(this.pool);
+    }
+
+    /**
+     * Apply a set of prefixed properties (represented as a list of entries) on a configuration object. Applying a
+     * prefixed property stands for reading a property, remove its prefix, and call the corresponding setters on the
+     * target configuration object.
+     *
+     * @param prefix the prefix.
+     * @param properties the properties.
+     * @param configObj the configuration object.
+     * @param <T> the type of the configuration object.
+     * @return the configured object.
+     */
+    private <T> T applyPrefixedConfig(final String prefix, final List<Entry<String, String>> properties,
+            final T configObj) {
+        properties.stream()
+            .filter(entry -> entry.getKey().startsWith(prefix))
+            .forEach(entry -> {
+                    final String configurationName = entry.getKey();
+
+                    try {
+                        BeanUtils.setProperty(configObj, StringUtils.removeStart(configurationName, prefix),
+                                entry.getValue());
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        LOGGER.error("Invalid configuration '{}'", configurationName, e);
+                    }
+                });
+
+        return configObj;
+    }
+
+    /**
+     * Borrows a database engine from the pool. Call {@link DatabaseEngine#close()} to return the borrowed database
+     * engine to pool.
+     *
+     * @return a database engine.
+     */
+    public DatabaseEngine borrow() {
+        // Log pool statistics every time we try to borrow a connection.
+        logStats();
+        try {
+            return pool.borrowObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Logs the number of active and idle pool connections.
+     */
+    private void logStats() {
+        // Avoid processing the logging strings if trace is disabled.
+        if (LOGGER.isTraceEnabled()) {
+            final int numActive = pool.getNumActive();
+            LOGGER.trace("{} Active Connections: {}", poolName, numActive);
+            LOGGER.trace("{} Idle Connections: {}", poolName, pool.getNumIdle());
+            LOGGER.trace("{} Total Connections: {}", poolName, maxTotal);
+
+            // warn the user if the number of active connections is equal to max pool size.
+            // this means that the application will be blocked waiting for DB.
+            if (numActive == maxTotal) {
+                LOGGER.trace("'{}' - WARNING: There isn't more connections on the pool. "
+                        + "The application will be blocked and it may perform poorly. "
+                        + "You may consider to increase the pool size, if this is a common occurrence.",
+                        poolName);
+                LOGGER.trace("'{}' - Check the documentation to learn how to do it.", poolName);
+            }
+        }
+    }
+
+    /**
+     * Closes the pool.
+     */
+    public void close() {
+        pool.close();
+    }
+
+    /**
+     * Gets the Pool JDBC string.
+     *
+     * @return the Pool JDBC string.
+     */
+    public String getPoolJdbc() {
+        return poolJdbc;
+    }
+
+    /**
+     * Gets the maximum number of connections.
+     *
+     * @return the maximum number of connections.
+     */
+    public int getMaxTotal() {
+        return maxTotal;
+    }
+
+    /**
+     * Gets the number of borrowed connections.
+     *
+     * @return the number of borrowed connections.
+     */
+    public int getNumActive() {
+        return pool.getNumActive();
+    }
+
+    /**
+     * Gets the pool name.
+     *
+     * @return the pool name.
+     */
+    public String getPoolName() {
+        return poolName;
+    }
+
+    /**
+     * Creates a new {@link DatabaseEnginePool}.
+     *
+     * @param properties the configured database and pool properties.
+     * @param engineModifier the database engine modifier to fit the application needs.
+     * @return a new DatabaseEnginePool.
+     */
+    public static DatabaseEnginePool getConnectionPool(final Map<String, String> properties,
+                                                final Consumer<DatabaseEngine> engineModifier) {
+        return new DatabaseEnginePool(properties, engineModifier);
+    }
+
+    /**
+     * Creates a new {@link DatabaseEnginePool}.
+     *
+     * @param properties the configured database and pool properties.
+     * @return a new DatabaseEnginePool.
+     */
+    public static DatabaseEnginePool getConnectionPool(final Map<String, String> properties) {
+        return new DatabaseEnginePool(properties, db -> {});
+    }
+
+    /**
+     * Creates a new {@link DatabaseEnginePool}.
+     *
+     * @param properties the configured database and pool properties.
+     * @param engineModifier the database engine modifier to fit the application needs.
+     * @return a new DatabaseEnginePool.
+     */
+    public static DatabaseEnginePool getConnectionPool(final Properties properties,
+                                                final Consumer<DatabaseEngine> engineModifier) {
+        return new DatabaseEnginePool(Maps.fromProperties(properties), engineModifier);
+    }
+
+    /**
+     * Creates a new {@link DatabaseEnginePool}.
+     *
+     * @param properties the configured database and pool properties.
+     * @return a new DatabaseEnginePool.
+     */
+    public static DatabaseEnginePool getConnectionPool(final Properties properties) {
+        return new DatabaseEnginePool(Maps.fromProperties(properties), db -> {});
+    }
+}
---
+++ src/main/java/com/feedzai/commons/sql/abstraction/engine/pool/PooledDatabaseEngineFactory.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2021 Feedzai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feedzai.commons.sql.abstraction.engine.pool;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.function.Consumer;
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+
+import com.feedzai.commons.sql.abstraction.engine.DatabaseEngine;
+import com.feedzai.commons.sql.abstraction.engine.DatabaseFactory;
+
+/**
+ * The factory of {@link PooledDatabaseEngine} instances.
+ *
+ * @author Luiz Silva (luiz.silva@feedzai.com)
+ * @author Francisco Santos (francisco.santos@feedzai.com)
+ * @since 2.8.3
+ */
+class PooledDatabaseEngineFactory extends BasePooledObjectFactory<PooledDatabaseEngine> {
+
+    /**
+     * The pool in which to pool {@link PooledDatabaseEngine}.
+     */
+    private ObjectPool<PooledDatabaseEngine> pool;
+
+    /**
+     * The properties needed to fabricate {@link DatabaseEngine} instances.
+     */
+    private final Properties properties;
+
+    /**
+     * The database engine modifier to fit the application needs.
+     */
+    private final Consumer<DatabaseEngine> engineModifier;
+
+    /**
+     * Creates a new {@link PooledDatabaseEngineFactory}.
+     *
+     * @param propertiesMap the configured database properties.
+     * {@link DatabaseEngine} instances and to configure the pool.
+     * @param engineModifier the database engine modifier to fit the application needs.
+     */
+     PooledDatabaseEngineFactory(final Map<String, String> propertiesMap,
+                                 final Consumer<DatabaseEngine> engineModifier) {
+        this.engineModifier = engineModifier;
+        // database connection properties.
+        this.properties = new Properties();
+
+        // load PDB connection properties from configuration file.
+        for (final Map.Entry<String, String> entry : propertiesMap.entrySet()) {
+            properties.setProperty(entry.getKey(), entry.getValue() == null ? "" : entry.getValue());
+        }
+    }
+
+    @Override
+    public PooledDatabaseEngine create() throws Exception {
+        final DatabaseEngine engine = DatabaseFactory.getConnection(properties);
+        // if defined, modify the engine.
+        if (engineModifier != null) {
+            // modify the engine to fit the application needs.
+            engineModifier.accept(engine);
+        }
+        return new PooledDatabaseEngine(pool, engine);
+    }
+
+    @Override
+    public boolean validateObject(final PooledObject<PooledDatabaseEngine> pooled) {
+        return pooled.getObject().checkConnection();
+    }
+
+    @Override
+    public PooledObject<PooledDatabaseEngine> wrap(final PooledDatabaseEngine engine) {
+        return new DefaultPooledObject<>(engine);
+    }
+
+    /**
+     * Gets the pool.
+     *
+     * @return the pool.
+     */
+    ObjectPool<PooledDatabaseEngine> getPool() {
+        return pool;
+    }
+
+    /**
+     * Sets the pool.
+     *
+     * @param pool the pool.
+     */
+    void setPool(final ObjectPool<PooledDatabaseEngine> pool) {
+        this.pool = pool;
+    }
+}
